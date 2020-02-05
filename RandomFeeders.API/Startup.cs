332e5caed431a1eab669f50acf1f43cefc8ab0fa
gabrielpda15 <gabriel.pda15@gmail.com>
2020-02-05 10:45:28 -0300

    Adicionando Cors e Cryptor nos services

--- RandomFeeders.API/Startup.cs
+++ RandomFeeders.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RandomFeeders.Security.Cryptography;
 
 namespace RandomFeeders.API
 {
@@ -22,15 +23,18 @@ namespace RandomFeeders.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
+
+            services.AddTransient<Cryptor>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().WithOrigins("localhost:4200"));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
