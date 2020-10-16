4004acb50ec2c8c68fab774c02dc84ba7867d54a
gabrielpda15 <gabriel.pda15@gmail.com>
2020-10-16 10:31:17 -0300

    v2.0.1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(`${__dirname}`));
+app.use(express.static(`${__dirname}/ngRandomFeeders`));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/ngRandomFeeders/index.html`));
